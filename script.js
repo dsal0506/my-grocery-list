adf5bf5f7cbff3421e5b1850b104f595ac405b75
dsal0506 <dsalinas_5605@stu.southtexascollege.edu>
2023-08-30 17:42:13 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const groceries = []
+  import { LocalDB } from 'https://cdn.skypack.dev/peadb';
+import shortid from 'https://cdn.skypack.dev/shortid';
 
-const groceryList = document.getElementById('groceryList')
-const newGroceryInput = document.getElementById('newGrocery')
-const addBtn = document.getElementById('addBtn')
+const db = new LocalDB('grocery-list-db');
+const groceries = db.getAll() || [];
+
+const groceryList = document.getElementById('groceryList');
+const newGroceryInput = document.getElementById('newGrocery');
+const addBtn = document.getElementById('addBtn');
 
 const createGroceryElement = (grocery) => {
-  const groceryElement = document.createElement('li')
-  groceryElement.innerText = grocery;
-  groceryElement.classList.add('groceryItem')
+  const groceryElement = document.createElement('li');
+  groceryElement.innerText = grocery.value;
+  groceryElement.classList.add('groceryItem');
   return groceryElement;
-}
+};
 
 const addGrocery = (newGrocery) => {
-  groceryList.appendChild(createGroceryElement(newGrocery))
-}
+  groceryList.appendChild(createGroceryElement(newGrocery));
+};
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const value = newGroceryInput.value;
-  if (value) {
-    addGrocery(value)
-    newGroceryInput.value = null // Clear the input field after adding
-  }
-})
 
-groceries.map(grocery => addGrocery(grocery))
+  if (value) {
+    const key = shortid.generate(); 
+    addGrocery({ key, value });
+    db.set(key, value);
+    newGroceryInput.value = ''; // Clear the input field after adding
+  }
+});
+
+groceries.forEach((grocery) => addGrocery(grocery)); 
